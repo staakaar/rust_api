a7a7377f4a03f6e01bb972321900c4b6f6377a30
staakaar <vancyroletaka@gmail.com>
2024-06-25 00:43:47 +0900

    change: log integration

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use actix_web::dev::Server;
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("world");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run() -> Result<Server, std::io::Error> {
-    // let body = async move {
-    //     HttpServer::new(|| {
-    //         App::new()
-    //             .route("/", web::get().to(greet))
-    //             .route("/{name}", web::get().to(greet))
-    //             .route("/health_chack", web::get().to(health_check))
-    //     })
-    //     .bind("127.0.0.1:8000")?
-    //     .run()
-    //     .await
-    // };
-
-    // tokio::runtime::Builder::new_multi_thread()
-    //     .enable_all()
-    //     .build()
-    //     .expect("Failed building the Runtime")
-    //     .block_on(body)
-    let server = HttpServer::new(|| {
-        App::new().route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run();
-
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+pub mod telemetry;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-use rust_api::run;
+use env_logger::Env;
+use rust_api::startup::run;
+use rust_api::telemetry::init_subscriber;
+use rust_api::{configuration::get_configuration, telemetry::get_subscriber};
+use sqlx::PgPool;
+use std::net::TcpListener;
+use tracing_log::LogTracer;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    // Logging
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    LogTracer::init().expect("Failed to set logger");
+
+    let subscriber = get_subscriber("rust_api".into(), "info".into());
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connnection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postges.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    run(listener, connnection)?.await
 }
