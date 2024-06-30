e2dbaa8b3d061dbb2ac80814713f7f1b1c77caff
staakaar <vancyroletaka@gmail.com>
2024-07-01 00:41:47 +0900

    add: api request validation

---
+++ src/domain.rs
@@ -0,0 +1,41 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+pub struct NewSubscriber {
+    pub email: String,
+    pub name: SubscriberName,
+}
+pub struct SubscriberName(pub String);
+
+impl SubscriberName {
+    pub fn parse(s: String) -> SubscriberName {
+        let is_empty_or_whitespace = s.trim().is_empty();
+        let is_too_long = s.graphemes(true).count() > 256;
+
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = s.chars().any(|c| forbidden_characters.contains(&c));
+
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            panic!("{} is not a valid subscriber name.", s)
+        } else {
+            Self(s)
+        }
+    }
+
+    pub fn inner(self) -> String {
+        self.0
+    }
+
+    pub fn inner_mut(&mut self) -> &mut str {
+        &mut self.0
+    }
+
+    // pub fn inner_ref(&self) -> &str {
+    //     &self.0
+    // }
+}
+
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
