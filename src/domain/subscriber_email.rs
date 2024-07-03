31fd8107e29306ce1b3d3871cfe0d74eb2bc5b80
staakaar <vancyroletaka@gmail.com>
2024-07-04 01:11:01 +0900

    add: domain unit test

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,9 +1,15 @@
+use validator::validate_email;
+
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
     pub fn parse(s: String) -> Result<SubscriberEmail, String> {
-        Ok(Self(s))
+        if validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
     }
 }
 
@@ -17,11 +23,28 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claims::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
 
     #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
-        assert_err!(SubscriberEmail::parse(email))
+        assert_err!(SubscriberEmail::parse(email));
     }
 
     #[test]
@@ -35,4 +58,10 @@ mod tests {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
+
+    // #[test]
+    // fn valid_emails_are_parsed_successfully() {
+    //     let email = SafeEmail.fake();
+    //     claims::assert_ok!(SubscriberEmail::parse(email));
+    // }
 }
