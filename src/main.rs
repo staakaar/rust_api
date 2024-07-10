5b58d6269702b6f1f816cf2959d1693c25f91e18
staakaar <vancyroletaka@gmail.com>
2024-07-11 00:26:44 +0900

    refactor: tests directory

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use env_logger::Env;
-use rust_api::email_client::{self, EmailClient};
-use rust_api::startup::run;
+use rust_api::startup::build;
 use rust_api::telemetry::init_subscriber;
 use rust_api::{configuration::get_configuration, telemetry::get_subscriber};
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use tracing_log::LogTracer;
 
 #[tokio::main]
@@ -18,31 +15,7 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    // let connection_pool =
-    //     PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-    //         .expect("Failed to create Postgres connection pool.");
-    let connection_pool =
-        PgPoolOptions::new().connect_lazy_with(configuration.database.without_db());
-
-    // EmailClient
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-
-    run(listener, connection_pool, email_client)?.await?;
+    let server = build(configuration).await?;
+    server.await?;
     Ok(())
 }
