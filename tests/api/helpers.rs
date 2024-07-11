d17075a2ccc0fb21934de64baca6e3f4b7fb9002
staakaar <vancyroletaka@gmail.com>
2024-07-12 00:27:58 +0900

    add: subscription test

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,11 +1,8 @@
 use once_cell::sync::Lazy;
-use rust_api::configuration::{get_configuration, DatabaseSettings};
-use rust_api::email_client::EmailClient;
-use rust_api::startup::build;
-use rust_api::startup::run;
+use rust_api::configuration::{self, get_configuration, DatabaseSettings};
+use rust_api::startup::{get_connection_pool, Application};
 use rust_api::telemetry::{get_subscriber, init_subscriber};
 use sqlx::{Connection, Executor, PgConnection, PgPool};
-use std::net::TcpListener;
 use uuid::Uuid;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -30,34 +27,41 @@ pub struct TestApp {
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.0.1:{}", port);
+    // let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // let port = listener.local_addr().unwrap().port();
+    // let address = format!("http://127.0.0.1:{}", port);
 
-    let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database).await;
+    let configuration = {
+        let mut c = get_configuration().expect("Faild to read configuration");
+        c.database.database_name = Uuid::new_v4().to_string();
+        c.application.port = 0;
+        c
+    };
 
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
+    configure_database(&configuration.database).await;
 
-    let timeout = configuration.email_client.timeout();
+    // let sender_email = configuration
+    //     .email_client
+    //     .sender()
+    //     .expect("Invalid sender email address.");
 
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
+    // let timeout = configuration.email_client.timeout();
 
-    let server =
-        run(listener, connection_pool.clone(), email_client).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
+    // let email_client = EmailClient::new(
+    //     configuration.email_client.base_url,
+    //     sender_email,
+    //     configuration.email_client.authorization_token,
+    //     timeout,
+    // );
+
+    let application = Application::build(configuration.clone())
+        .await
+        .expect("Failed to bind address");
+    let address = format!("http://127.0.0.1:{}", application.port());
+    let _ = tokio::spawn(application.run_until_stopped());
     TestApp {
         address,
-        db_pool: connection_pool,
+        db_pool: get_connection_pool(&configuration.database),
     }
 }
 
