cafa295c29bb2dee357698998df51fa95e55d91c
staakaar <vancyroletaka@gmail.com>
2024-07-31 00:03:01 +0900

    add: cookie setup and login redirect

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,3 +1,4 @@
+use actix_web::cookie::Cookie;
 use actix_web::error::InternalError;
 use actix_web::http::header::{ContentType, LOCATION};
 use actix_web::http::StatusCode;
@@ -25,11 +26,11 @@ pub enum LoginError {
     UnexpectedError(#[from] anyhow::Error),
 }
 
-#[tracing::instrument(skip(form, pool, secret), fields(username=tracing::field::Empty, user_id=tracing::field::Empty))]
+#[tracing::instrument(skip(form, pool), fields(username=tracing::field::Empty, user_id=tracing::field::Empty))]
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    secret: web::Data<HmacSecret>,
+    // secret: web::Data<HmacSecret>,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -49,21 +50,20 @@ pub async fn login(
                 AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
                 AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
             };
-            let query_string = format!("error={}", urlencoding::Encoded::new(e.to_string()));
+            // let query_string = format!("error={}", urlencoding::Encoded::new(e.to_string()));
 
-            let hmac_tag = {
-                let mut mac =
-                    Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes())
-                        .unwrap();
-                mac.update(query_string.as_bytes());
-                mac.finalize().into_bytes()
-            };
+            // let hmac_tag = {
+            //     let mut mac =
+            //         Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes())
+            //             .unwrap();
+            //     mac.update(query_string.as_bytes());
+            //     mac.finalize().into_bytes()
+            // };
 
             let response = HttpResponse::SeeOther()
-                .insert_header((
-                    LOCATION,
-                    format!("/login?{}&tag={:x}", query_string, hmac_tag),
-                ))
+                .insert_header((LOCATION, "/login"))
+                .cookie(Cookie::new("_flash", e.to_string()))
+                // .insert_header(("Set-Cookie", format!("_flash={e}")))
                 .finish();
             Err(InternalError::from_response(e, response))
         }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -35,6 +35,7 @@ pub struct TestApp {
     pub db_pool: PgPool,
     pub email_server: MockServer,
     test_user: TestUser,
+    api_client: reqwest::Client,
 }
 
 pub struct TestUser {
@@ -77,7 +78,7 @@ impl TestUser {
 
 impl TestApp {
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
-        reqwest::Client::new()
+        self.api_client
             .post(&format!("{}/subdcriptions", &self.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(body)
@@ -87,13 +88,41 @@ impl TestApp {
     }
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
-        reqwest::Client::new()
+        self.api_client
             .post(&format!("{}/newsletters", &self.address))
             .basic_auth(&self.test_user.username, Some(&self.test_user.password))
             .json(&body)
             .send()
             .await
             .expect("Failed to execute request.")
+    }
+
+    pub async fn post_login<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
+        reqwest::Client::builder()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .unwrap();
+
+        // self.api_client
+        //     .post(&format!("{}/login", &self.address))
+        //     .form(body)
+        //     .send()
+        //     .await
+        //     .expect("Failed to execute request.")
+    }
+
+    pub async fn get_login_html(&self) -> String {
+        self.api_client
+            .get(&format!("{}/login", &self.address))
+            .send()
+            .await
+            .expect("Faield to execute request.")
+            .text()
+            .await
+            .unwrap()
     }
 
     pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
@@ -166,12 +195,20 @@ pub async fn spawn_app() -> TestApp {
     let address = format!("http://127.0.0.1:{}", application.port());
     let application_port = application.port();
     let _ = tokio::spawn(application.run_until_stopped());
+
+    let client = reqwest::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .cookie_store(true)
+        .build()
+        .unwrap();
+
     let test_app = TestApp {
         address,
         port: application_port,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
         test_user: TestUser::generate(),
+        api_client: client,
     };
 
     test_app.test_user.store(&test_app.db_pool).await;
@@ -210,4 +247,9 @@ async fn add_test_user(pool: &PgPool) {
     .execute(pool)
     .await
     .expect("Failed to create test users.");
+}
+
+pub fn assert_is_redirect_to(response: &reqwest::Response, location: &str) {
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), location);
 }
